a4255c7ccdb39911e2408653a85190adfe4e9010
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2022-12-21 21:01:11 -0300

    Update pages.js

--- _js/pages.js
+++ _js/pages.js
@@ -1,13 +1,20 @@
-// criando o obj do pagelabel
+// definindo const uteis
 const label = document.getElementById("pagelabel");
+const title = document.getElementById("title");
+const description = document.getElementById("title-text");
+const img1 = document.getElementById("img1");
 
 // função para mudança das pages
 const homePage = () => {
     label.innerText = "Home";
+    title.removeAttribute("hidden","");
+    description.innerText = "Hi, my name is João Nunes, also know as nunesdotpy, I am a brazilian computational science student who loves front-end development, blockchain and decentralized projects.";
 }
 
 const workPage = () => {
     label.innerText = "Work";
+    title.setAttribute("hidden","");
+    description.innerText = "Here you see some of my projects that a have developed in the past";
 }
 
 const contactPage = () => {
