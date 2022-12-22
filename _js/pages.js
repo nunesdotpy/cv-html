3260cfc68905b2229b5ddc4456f739d86225c1d1
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2022-12-21 21:13:41 -0300

    Update pages.js

--- _js/pages.js
+++ _js/pages.js
@@ -7,12 +7,14 @@ const img1 = document.getElementById("img1");
 // função para mudança das pages
 const homePage = () => {
     label.innerText = "Home";
+    img1.removeAttribute("hidden","");
     title.removeAttribute("hidden","");
     description.innerText = "Hi, my name is João Nunes, also know as nunesdotpy, I am a brazilian computational science student who loves front-end development, blockchain and decentralized projects.";
 }
 
 const workPage = () => {
     label.innerText = "Work";
+    img1.setAttribute("hidden","");
     title.setAttribute("hidden","");
     description.innerText = "Here you see some of my projects that a have developed in the past";
 }
