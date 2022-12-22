146fdaa2761715cfe0cbd2bb1f804ef28d2e2c50
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2022-12-22 04:22:52 -0300

    função para mudar o work

---
+++ _js/work.js
@@ -0,0 +1,6 @@
+const nextWork = () => {
+    title.innerText = "UniDev";
+    description.innerText = `Site para ajudar pessoas iniciantes no mundo de desenvolvimento web.`;
+    clickhere.setAttribute("href", "https://dev.nunesdotpy.com");
+    img.setAttribute("src", "_img/work2.jpeg");
+}
