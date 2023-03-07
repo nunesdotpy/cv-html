76ef674db4a6252c09eb6b098c6c1c267eb0221e
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2023-03-07 04:57:09 -0300

    criando um /cv

--- _js/pages.js
+++ _js/pages.js
@@ -7,7 +7,6 @@ const email = "contato@nunesdotpy.com";
 clickhere.innerText = "Clique aqui para visitar";
 let workShowing = 0;
 
-
 // page 0 = home, 2 = work e 3 = contato
 let page = 0;
 workShowing = 0;
@@ -15,11 +14,11 @@ workShowing = 0;
 // criando elementos
 const mainframe = document.querySelector(".cabecalho");
 const contatos = document.createElement("div");
-contatos.setAttribute("class","social-medias");
+contatos.setAttribute("class", "social-medias");
 contatos.style.display = "none";
 const imagens = document.createElement("div");
 imagens.setAttribute("class", "imagens");
-const img = document.createElement("img")
+const img = document.createElement("img");
 const txtPage = document.createElement("p");
 const arrow = document.createElement("img");
 const homeSvg = document.getElementById("home");
@@ -39,54 +38,55 @@ img.setAttribute("alt", "nunesdotpy github photo");
 // arrow do work
 arrow.setAttribute("id", "arrow");
 arrow.setAttribute("src", "assets/arrow.svg");
-arrow.setAttribute("onclick", "nextWork()")
+arrow.setAttribute("onclick", "nextWork()");
 arrow.style.display = "none";
 
 // função para mudança das pages
 const homePage = () => {
-    page = 0;
-    opacityReset();
-    label.innerText = "Home";
-    img.setAttribute("id", "img");
-    img.setAttribute("src", "_img/github-photo.jpeg");
-    img.setAttribute("alt", "nunesdotpy github photo");
-    title.innerText = "João Nunes";
-    description.innerText = "Oi, meu nome é João Nunes, também conhecido como nunesdotpy, sou um recifense graduante em ciência da computação. Adoro desenvolvimento front-end, projetos open-source e descentralizados! Aqui você vai conhecer um pouco mais sobre mim. Atualmente meu foco é em conquistar uma vaga de Dev Junior. Tenho domínio das tecnologias de HTML, CSS, JavaScript, Bootstrap e Git. Meu Inglês é avançado, tenho experiência com NodeJS e frameworks como ReactJS e Angular para desenvolvimento de soluções full-stack. Também tenho conhecimento e já desenvolvi projetos em C, PHP, Java, Python e utilização da metodologia SCRUM.";
-    txtPage.style.display = "none";
-    arrow.style.display = "none";
-    clickhere.style.display = "none";
-    imagens.style.display = "block";
-    title.style.display = "block";
-    description.style.display = "block";
-    contatos.style.display = "none";
-}
+  page = 0;
+  opacityReset();
+  label.innerText = "Home";
+  img.setAttribute("id", "img");
+  img.setAttribute("src", "_img/github-photo.jpeg");
+  img.setAttribute("alt", "nunesdotpy github photo");
+  title.innerText = "João Nunes";
+  description.innerText =
+    "Oi, meu nome é João Nunes, também conhecido como nunesdotpy, sou um recifense graduante em ciência da computação. Adoro desenvolvimento front-end, projetos open-source e descentralizados! Aqui você vai conhecer um pouco mais sobre mim. Atualmente meu foco é em conquistar uma vaga de Dev Junior. Tenho domínio das tecnologias de HTML, CSS, JavaScript, Bootstrap e Git. Meu Inglês é avançado, tenho experiência com NodeJS e frameworks como ReactJS e Angular para desenvolvimento de soluções full-stack. Também tenho conhecimento e já desenvolvi projetos em C, PHP, Java, Python e utilização da metodologia SCRUM.";
+  txtPage.style.display = "none";
+  arrow.style.display = "none";
+  clickhere.style.display = "none";
+  imagens.style.display = "block";
+  title.style.display = "block";
+  description.style.display = "block";
+  contatos.style.display = "none";
+};
 
 const workPage = () => {
-    page = 1;
-    opacityReset();
-    label.innerText = "Trabalhos";
-    img.removeAttribute("id", "img")
-    img.setAttribute("class", "works");
-    txtPage.style.display = "none";
-    arrow.style.display = "block";
-    clickhere.style.display = "flex"
-    imagens.style.display = "block";
-    title.style.display = "block";
-    description.style.display = "block";
-    contatos.style.display = "none";
-    work();
-}
+  page = 1;
+  opacityReset();
+  label.innerText = "Trabalhos";
+  img.removeAttribute("id", "img");
+  img.setAttribute("class", "works");
+  txtPage.style.display = "none";
+  arrow.style.display = "block";
+  clickhere.style.display = "flex";
+  imagens.style.display = "block";
+  title.style.display = "block";
+  description.style.display = "block";
+  contatos.style.display = "none";
+  work();
+};
 
 const contactPage = () => {
-    page = 2;
-    opacityReset();
-    clickhere.style.display = "none";
-    label.innerText = "Contatos";
-    imagens.style.display = "none";
-    title.style.display = "none";
-    description.style.display = "none";
-    contatos.style.display = "block";
-    contatos.innerHTML = `
+  page = 2;
+  opacityReset();
+  clickhere.style.display = "none";
+  label.innerText = "Contatos";
+  imagens.style.display = "none";
+  title.style.display = "none";
+  description.style.display = "none";
+  contatos.style.display = "block";
+  contatos.innerHTML = `
         <div class="social">
             <a class="email" href="#" onclick="copiarEmail()">
                 <img ed src="../_img/gmail.png"></img>
@@ -109,46 +109,46 @@ const contactPage = () => {
             </a>
         </div>
     
-    `
-}
+    `;
+};
 
 const copiarEmail = () => {
-    navigator.clipboard.writeText(email);
-}
+  navigator.clipboard.writeText(email);
+};
 
 const opacityReset = () => {
-    switch (page){
-        case 0:
-            homeSvg.style.opacity = 0.5;
-            workSvg.style.opacity = 1;
-            contactSvg.style.opacity = 1;
-            break;
-        case 1:
-            homeSvg.style.opacity = 1;
-            workSvg.style.opacity = 0.5;
-            contactSvg.style.opacity = 1;
-            break;
-        case 2:
-            homeSvg.style.opacity = 1;
-            workSvg.style.opacity = 1;
-            contactSvg.style.opacity = 0.5;
-            break;
-    }
-}
+  switch (page) {
+    case 0:
+      homeSvg.style.opacity = 0.5;
+      workSvg.style.opacity = 1;
+      contactSvg.style.opacity = 1;
+      break;
+    case 1:
+      homeSvg.style.opacity = 1;
+      workSvg.style.opacity = 0.5;
+      contactSvg.style.opacity = 1;
+      break;
+    case 2:
+      homeSvg.style.opacity = 1;
+      workSvg.style.opacity = 1;
+      contactSvg.style.opacity = 0.5;
+      break;
+  }
+};
 
 const imgScript = () => {
-    img.setAttribute("src", "_img/work1.jpeg");
-    img.setAttribute("src", "_img/work2.jpeg");
-    img.setAttribute("src", "_img/work3.jpeg");
-    img.setAttribute("src", "_img/work4.jpeg");
-}
+  img.setAttribute("src", "_img/work1.jpeg");
+  img.setAttribute("src", "_img/work2.jpeg");
+  img.setAttribute("src", "_img/work3.jpeg");
+  img.setAttribute("src", "_img/work4.jpeg");
+};
 
 const execScripts = () => {
-    contactPage();
-    imgScript();
-    homePage();
-    opacityReset();
-}
+  contactPage();
+  imgScript();
+  homePage();
+  opacityReset();
+};
 
 // melhorar um pouco o desempenho dos scripts
 execScripts();
