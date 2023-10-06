df9d2e47936ab89156eaf0634848255842494464
Joao Nunes <contato@nunes.sh>
2023-10-06 02:08:42 -0300

    new portfolio

---
+++ _js/router.js
@@ -0,0 +1,23 @@
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+};
+const routes = {
+  404: "/pages/home.html",
+  "/": "/pages/home.html",
+  "/trabalhos": "/pages/trabalhos.html",
+  "/contato": "/pages/contato.html",
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("main-page").innerHTML = html;
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
