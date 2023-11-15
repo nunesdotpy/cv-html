57f632299ae2e08b9d31f04d490789257311482c
João Nunes <contato@nunes.sh>
2023-11-15 00:23:15 -0300

    pagina work feita

--- js/index.js
+++ js/index.js
@@ -1,15 +1,19 @@
 const Navbar = (pagina) => {
     document.getElementById('home').style.color = "#848588";
+    document.querySelector('.home').hidden = true;
     document.getElementById('work').style.color = "#848588";
+    document.querySelector('.work').hidden = true;
     document.getElementById('contact').style.color = "#848588";
 
 
     switch (pagina) {
         case 'home':
             document.getElementById('home').style.color = "white";
+            document.querySelector('.home').hidden = false;
             break;
         case 'work':
             document.getElementById('work').style.color = "white";
+            document.querySelector('.work').hidden = false;
             break;
         case 'contact':
             document.getElementById('contact').style.color = "white";
