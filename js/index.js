ba5dd074a4c2b1fb4c35bd8a965093fd6adde0ba
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2023-11-17 14:54:54 -0300

    responsividade na mao (undo)

--- js/index.js
+++ js/index.js
@@ -22,28 +22,6 @@ const Navbar = (pagina) => {
   }
 };
 
-fetch("/js/works.json").then((response) => {
-  response.json().then((dados) => {
-    const work = document.querySelector(".works");
-
-    dados.projetos.forEach((projeto) => {
-      work.innerHTML += `
-          <div class="work_card" id="work_${projeto.id}" onclick="verMais(${projeto.id})">
-            <img src="${projeto.imagem}" width="300" height="170" alt="${projeto.imagemAlt}">
-            <h2>${projeto.titulo}</h2>
-            <p id="verMais_${projeto.id} class="verMais">Ver Mais <i class="bi bi-arrow-right-square-fill"></i></p>
-            <div hidden id="${projeto.id}">
-              <p class="descricao">${projeto.descricao}</p>
-              <div style="display: flex; justify-content: center">
-                <a href="${projeto.link}" target="_blank" class="link">Acessar projeto</a>
-              </div>
-            </div>
-          </div>
-        `;
-    });
-  });
-});
-
 const verMais = (projeto) => {
   document.getElementById(projeto).style.display = "block";
   document.getElementById(`work_${projeto}`).style.height = "460px";
