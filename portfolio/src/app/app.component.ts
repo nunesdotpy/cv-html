0faf4d6a0f0c74916e15254de21da8c03cf569f8
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2024-04-12 03:06:13 -0300

    [add] import contato

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet  } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,13 @@ import { HomeComponent } from './home/home.component';
   imports: [
     RouterOutlet,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    ContatoComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'portfolio';
 }
