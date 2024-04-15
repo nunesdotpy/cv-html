49f5caafcf649dbdda25d355762cb6cbdf875e4e
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2024-04-15 12:34:04 -0300

    Update app.routes.ts

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { ContatoComponent } from './contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'contato',
+    component: ContatoComponent,
+  },
+];
+
+const routing: ModuleWithProviders = RouterModule.forRoot(routes);
