5db9b737f7d7757278b72c176fa89cc37e1a6b4f
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2024-05-01 04:06:30 -0300

    [feat] routerModule

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet  } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ContatoComponent } from './contato/contato.component';
+import { RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
     NavbarComponent,
     HomeComponent,
-    ContatoComponent
+    ContatoComponent,
+    RouterModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
