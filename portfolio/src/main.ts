520632c3612773d0f692166d3b17b7a99bb42f32
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2024-05-01 04:05:40 -0300

    [feat] rotas

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import routeConfig from './app/app.routes';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routeConfig)
+  ]
+}).then(() => {
+  console.log('Application has been bootstrapped!');
+});
